45464abd991c38328e13ac0effa0380b3d3abf5a
Joao-hey <joaoribcorreia@gmail.com>
2022-03-07 11:57:46 +0100

    changed php to index

--- code/index.php
+++ code/index.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="controller/Start_website.css">
+    <link rel="stylesheet" href="/152_modul/code/client/Style/Start_website.css">
 </head>
 
 <body>
     <div class="box">
         <div class="box-header">
 
-            <img class="logo" src="img/logo.png" alt="">
+            <img class="logo" src="/152_modul/code/client/img/logo.png" alt="">
 
             <div class="button">
                 <a href="">anmelden</a>
@@ -24,8 +24,8 @@
         <div class="box-underliner">
 
             <picture>
-                <source srcset="img/line-small.png" media="(max-width: 700px)">
-                <img src="img/line-big.png" alt="">
+                <source srcset="/152_modul/code/client/img/line-small.png" media="(max-width: 700px)">
+                <img src="/152_modul/code/client/img/line-big.png" alt="">
             </picture>
         </div>
 
--- code/server/presentation/helper/PageBuilder.php
+++ code/server/presentation/helper/PageBuilder.php
@@ -5,77 +5,51 @@ class PageBuilder
     public static function pagebuild($title = "", $pageKey = "")
     {
         echo "<!DOCTYPE html>
-            <html lang=\"en\">
-                <head>
-                    <meta charset=\"UTF-8\">
-                    <title>$title</title>
-                    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-                    <meta name=\"viewport\" content=\"width=K, initial-scale=1.0\">
-                    <link rel=\"stylesheet\"  href=\"app/client/view/styles/Style.css\">
-                </head>
-                <body>
-                    <div class=\"link-container\">
-                        <table class=\"link\" >
-                            <tr class=\"link-button\">
-                                <td" . ($title == "Startseite" ? " class=\"active\" " : "") . ">
-                                    <a class=\"logo\" href=\"Startseite.php\">
-                                        <img src=\"app/client/view/images/header.jpg\" alt=\"logo\">
-                                    </a>
-                                </td>
-                                
-                                
-
-                                <td" . ($title == "Empfehlung" ? " class=\"active\" " : "") . ">
-                                    <a class=\"link-button-text\" href=\"Empfehlungen.php\">Gespräche</a>
-                                </td>
-
-
-                                <td" . ($title == "Profil" ? " class=\"active\" " : "") . ">
-                                    <a class=\"link-button-text\" href=\"Profile.php\">Profil</a>
-                                </td>
-
-                                <td class=\"extra\" >
-                                    <p>Day:     
-                                    <script>
-                                        var today = new Date();
-                                        var time = today.getFullYear() + \"-\" + (today.getMonth()+1) + \"-\" +  today.getDate() + \"<br>\" + today.getHours() + \":\" + today.getMinutes() + \"uhr\";
-                                        document.write(time);;
-                                    </script> 
-                                    </p>
-                                </td>
-
-                            </tr>
-
-                        </table>
-
+        <html lang=\"en\">
+        
+        <head>
+            <meta charset=\"UTF-8\">
+            <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+            <title>Document</title>
+            <link rel=\"stylesheet\" href=\"controller/Start_website.css\">
+        </head>
+        
+        <body>
+            <div class=\"box\">
+                <div class=\"box-header\">
+        
+                    <img class=\"logo\" src=\"img/logo.png\" alt=\"\">
+        
+                    <div class=\"button\">
+                        <a href=\"\">anmelden</a>
+                        <a href=\"\">registieren</a>
                     </div>
-                
-                    <div class=\"content-container\">
-                        <div class=\"content-body\">
-                            <div class=\"content\">
-    
-                            
-                            ";
+                </div>
+        
+                <div class=\"box-underliner\">
+        
+                    <picture>
+                        <source srcset=\"img/line-small.png\" media=\"(max-width: 700px)\">
+                        <img src=\"img/line-big.png\" alt=\"\">
+                    </picture>
+                </div>
+        
+                <div class=\"box-body\">
+                <div>";
 
         include($_SERVER["DOCUMENT_ROOT"] . "/Projekt_ouk307-main/htdocs/app/server/presentation/view/" . $pageKey .".php");
 
 
         echo "
 
-                            </div>
-                            <div class=\"content-preseberator\">
-                                    <img src=\"app/client/view/images/Bild-" . rand(1, 4) . ".png\" alt=\"cool photo missing\">
-                            </div>
-                        </div>
+        </div>
+        </div>
 
-                        <div class=\"footer\">
-                            <span>&copy " . date("Y") . " Jc gmbh</span>" . " &#183 " . "<a href=\"Datenschutz.php\">Datenschutz</a>
-                        </div> 
-                            
-                    </div>
-                    
-                </body>
-            </html>
+    </div>
+</body>
+
+</html>
 ";
     }
 }
